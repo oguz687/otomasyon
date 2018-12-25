5ddf2d323d9ee34f995660f914a75b7ae8d56d47
oguz687 <oguzhan_687@hotmail.com>
2018-12-26 00:26:42 +0300

    çalışan tamamlanmamış sürüm

--- tests/test_otomasyon.py
+++ tests/test_otomasyon.py
@@ -109,7 +109,7 @@ class Test_SayfaGetir():
         if "veri" in sorgu:
             assert sorgu == {"veri","43"}
 
-    def test_database_get(self):
+    def test_database_get(self):         # veritabanını test eder
         orn=Veritabani()
         orn.sayfa_ekle(({"veri","43"}))
         sayfa=orn.sayfa_getir("veri")
